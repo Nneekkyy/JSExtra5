57ff722cfd5ff1e3e2d51df9d487f2021145c70d
Alexey Krasnov <neky@list.ru>
2021-01-20 15:15:37 +0500

    123

--- scripts/index1.js
+++ scripts/index1.js
@@ -6,54 +6,34 @@ const cardContainer = document.querySelector('.hotel');
 const minSortButton = document.querySelector('.button-sort__down');
 const maxSortButton = document.querySelector('.button-sort__up');
 //новый массив
-const mapHotels = hotels.map (function (element) {
-  return element
-});
+
 let sortMethod = false;
 //сортировка по возрастанию
 function sortToggle () {
   if (sortMethod) {
-      mapHotels.sort((a, b) => b.price - a.price);
+      hotels.sort((a, b) => b.price - a.price);
   } else {
-      mapHotels.sort((a, b) => a.price - b.price);
+    hotels.sort((a, b) => a.price - b.price);
   }
-
-  return mapHotels;
+  return hotels;
 }
 
 
 //функция создания карточки
-function createHotelCard(title, price, photo) {
-  console.log(price);
+const createHotelCard = (hotel) => {
+  console.log(hotel);
   const cardElement = cardTemplate.cloneNode(true);
-  cardElement.querySelector('.hotel__img').src = photo;
-  cardElement.querySelector(".hotel__name").textContent = title;
-  cardElement.querySelector(".hotel__price-value").textContent = price;
+  const hotelPhoto = cardElement.querySelector('.hotel__img');
+  const hotelTitle = cardElement.querySelector('.hotel__name');
+  const hotelPrice = cardElement.querySelector('.hotel__price-value');
+  hotelPhoto.src = hotel.photo;
+  hotelTitle.textContent = hotel.title;
+  hotelPrice.textContent = hotel.price;
+
   return cardElement;
 }
 //добавление карточки, если в массиве есть данные
-function addCard(title, price, photo) {
-    if (price && title && photo) {
-      cardContainer.append(createHotelCard(title, price, photo));
-    }
-}
-//создание карточек из массива
-sortToggle().forEach(function (card) {
-  addCard(card.title, card.price, card.photo);
-});
-
-minSortButton.addEventListener('click', function () {
-  cardContainer.remove();
-    sortMethod = true;
-  sortToggle().forEach(function (card) {
-    addCard(card.title, card.price, card.photo);
-  });
-});
-maxSortButton.addEventListener('click', function () {
-  cardContainer.remove();
-    sortMethod = false;
-  mapHotels.forEach(function (card) {
-    addCard(card.title, card.price, card.photo);
-  });
-});
-console.log(sortMethod);
+const renderHotels = () => {
+  hotels.forEach(item => cardContainer.append(createHotelCard(item)));
+};
+renderHotels();
