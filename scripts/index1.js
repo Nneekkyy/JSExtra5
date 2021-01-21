72392df4c556cde849647b85c4867ff25e5fa23c
Alexey Krasnov <neky@list.ru>
2021-01-21 16:43:44 +0500

    123

--- scripts/index1.js
+++ scripts/index1.js
@@ -5,19 +5,13 @@ const cardTemplate = document.querySelector('#hotels').content;
 const cardContainer = document.querySelector('.hotel');
 const minSortButton = document.querySelector('.button-sort__down');
 const maxSortButton = document.querySelector('.button-sort__up');
-//новый массив
-
-let sortMethod = false;
-//сортировка по возрастанию
-function sortToggle () {
-  if (sortMethod) {
-      hotels.sort((a, b) => b.price - a.price);
-  } else {
-    hotels.sort((a, b) => a.price - b.price);
-  }
-  return hotels;
+//состояние
+const appState = {
+  sortMethod: undefined,
+  visibleHotels: 3,
+  hotels: [],
+  isPopupOpen: false,
 }
-
 
 //функция создания карточки
 const createHotelCard = (hotel) => {
@@ -32,8 +26,24 @@ const createHotelCard = (hotel) => {
 
   return cardElement;
 }
+//сортировка по возрастанию
+function sortToggle () {
+  if (appState.sortMethod) {
+    hotels.sort((a, b) => b.price - a.price);
+  } else {
+    hotels.sort((a, b) => a.price - b.price);
+  }
+  return hotels;
+}
+
+const validateHotels = () => {
+  appState.hotels = hotels.filter(item => item.price).filter(item => item.photo);
+};
+
+
 //добавление карточки, если в массиве есть данные
 const renderHotels = () => {
-  hotels.forEach(item => cardContainer.append(createHotelCard(item)));
+  validateHotels();
+  appState.hotels.forEach(item => cardContainer.append(createHotelCard(item)));
 };
 renderHotels();
