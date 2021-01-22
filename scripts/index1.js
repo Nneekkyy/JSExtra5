2bdd91f6ed3bfab85c8f1583161f32df8f34c2cd
Alexey Krasnov <neky@list.ru>
2021-01-22 14:55:25 +0500

    123

--- scripts/index1.js
+++ scripts/index1.js
@@ -2,13 +2,17 @@
 import {hotels} from './HotelData.js';
 
 const cardTemplate = document.querySelector('#hotels').content;
+const popup = document.querySelector('.popup');
 const cardContainer = document.querySelector('.hotel');
 const minSortButton = document.querySelector('.button-sort__down');
 const maxSortButton = document.querySelector('.button-sort__up');
+const showMinButton = document.querySelector('#buttonjs');
+const showMoreButton = document.querySelector('#buttoncss');
+const closeButton = document.querySelector('.popup__close');
 //состояние
 const appState = {
   sortMethod: undefined,
-  visibleHotels: 3,
+  visibleHotels: 2,
   hotels: [],
   isPopupOpen: false,
 }
@@ -30,20 +34,59 @@ const createHotelCard = (hotel) => {
 function sortToggle () {
   if (appState.sortMethod) {
     hotels.sort((a, b) => b.price - a.price);
+    renderHotels();
   } else {
     hotels.sort((a, b) => a.price - b.price);
+    renderHotels();
   }
-  return hotels;
+  return appState.hotels;
 }
 
 const validateHotels = () => {
   appState.hotels = hotels.filter(item => item.price).filter(item => item.photo);
 };
 
-
 //добавление карточки, если в массиве есть данные
 const renderHotels = () => {
+  cardContainer.textContent = '';
   validateHotels();
-  appState.hotels.forEach(item => cardContainer.append(createHotelCard(item)));
+  const hotelsToRender = appState.hotels.slice(0, appState.visibleHotels)
+  hotelsToRender.forEach(item => cardContainer.append(createHotelCard(item)));
 };
+const hideButton = () => {
+  showMinButton.classList.add('button-more_hide');
+  showMoreButton.classList.add('button-more_hide');
+}
 renderHotels();
+
+const addAppStateVisible = () => {
+  appState.visibleHotels++;
+}
+showMinButton.addEventListener('click', function () {
+  if (appState.hotels.length!=appState.visibleHotels) {
+    addAppStateVisible();
+    renderHotels();
+  } else {
+    hideButton();
+  }
+});
+
+showMoreButton.addEventListener('click', function () {
+    appState.visibleHotels = appState.hotels.length;
+    addAppStateVisible();
+    renderHotels();
+    hideButton();
+})
+
+minSortButton.addEventListener('click', function () {
+  appState.sortMethod = true;
+  sortToggle();
+});
+maxSortButton.addEventListener('click', function () {
+  appState.sortMethod = false;
+  sortToggle();
+});
+const closePopup = () => {
+  popup.classList.remove('popup_opened');
+};
+closeButton.addEventListener('click', closePopup)
